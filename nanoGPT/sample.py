7fe7bcd66ec1142817b3a7959fe56ebebdd4d0e5
Pratik Wadekar <pratik@Pratiks-MacBook-Air-5.local>
2026-01-25 19:26:12 +0530

    Change

--- nanoGPT/sample.py
+++ nanoGPT/sample.py
@@ -23,7 +23,7 @@ temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, i
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
 # device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-device = 'mps' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 # dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 dtype = 'float32'
 compile = False # use PyTorch 2.0 to compile the model to be faster
@@ -93,7 +93,7 @@ with torch.no_grad():
     with ctx:
         for k in range(num_samples):
             # model.clear_kv_cache()
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k,use_cache=False)
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k,use_cache=True)
             print(decode(y[0].tolist()))
             print('---------------')
     end = time.time()
