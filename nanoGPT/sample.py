b44eea679b2879e042328f3d292a213a85d3da09
Pratik Wadekar <pratik@Pratiks-MacBook-Air-5.local>
2026-02-09 11:50:16 +0530

    changes

--- nanoGPT/sample.py
+++ nanoGPT/sample.py
@@ -11,7 +11,7 @@ import time
 
 
 # -----------------------------------------------------------------------------
-init_from = 'gpt2-medium' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+init_from = 'gpt2-xl' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
 # start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 start = "Android is a \n"
@@ -23,7 +23,7 @@ temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, i
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
 # device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-device = 'mps' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 # dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 dtype = 'float32'
 compile = False # use PyTorch 2.0 to compile the model to be faster
